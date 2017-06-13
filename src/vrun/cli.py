d02ecf00fe23650808bc7a45a8ac852d515e444d
Bert JW Regeer <bertjw@regeer.org>
2017-06-13 00:36:12 -0600

    Add Windows support

--- src/vrun/cli.py
+++ src/vrun/cli.py
@@ -2,9 +2,21 @@ from __future__ import print_function
 import os
 import sys
 
+def get_binpath(prefix):
+    if os.name == 'nt':
+        return os.path.join(prefix, 'Scripts')
+    else:
+        return os.path.join(prefix, 'bin')
+
+def get_exec(binpath, execbin):
+    if os.name == 'nt' and '.exe' not in execbin:
+        return os.path.join(binpath, '{}.exe'.format(execbin))
+    else:
+        return os.path.join(binpath, execbin)
+
 def main():
     prefix = sys.prefix
-    binpath = os.path.join(prefix, 'bin')
+    binpath = get_binpath(prefix)
 
     PATH = os.environ.get('PATH', '')
     if PATH:
@@ -28,7 +40,7 @@ def main():
     execbin = newargv[0]
 
     if os.sep not in execbin:
-        execbin = os.path.join(binpath, execbin)
+        execbin = get_exec(binpath, execbin)
 
     if not os.path.exists(execbin):
         print('vrun requires that the target executable exists.', file=sys.stderr)
@@ -36,8 +48,21 @@ def main():
         sys.exit(-1)
 
     try:
-        # Execute the actual executable...
-        os.execve(execbin, newargv, newenv)
+        # Windows has to be special...
+        if os.name == 'nt':
+            import subprocess
+            exe = subprocess.Popen(
+                [execbin] + newargv[1:],
+                env=newenv,
+                universal_newlines=True
+            )
+            exe.communicate()
+            sys.exit(exe.returncode)
+        else:
+            # Sane OS's let you just execute a new binary that takes the place
+            # of the old:
+            os.execve(execbin, newargv, newenv)
+
     except Exception as e:
         print('vrun was unable to execute the target executable.', file=sys.stderr)
         print('Executable: {}'.format(execbin), file=sys.stderr)
