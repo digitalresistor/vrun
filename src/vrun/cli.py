700b1d89c79869c882a93e61f86df2f8497bd0b3
Bert JW Regeer <bertjw@regeer.org>
2017-09-16 14:34:11 -0600

    Move operating system specific functions

--- src/vrun/cli.py
+++ src/vrun/cli.py
@@ -2,17 +2,11 @@ from __future__ import print_function
 import os
 import sys
 
-def get_binpath(prefix):
-    if os.name == 'nt':
-        return os.path.join(prefix, 'Scripts')
-    else:
-        return os.path.join(prefix, 'bin')
-
-def get_exec(binpath, execbin):
-    if os.name == 'nt' and '.exe' not in execbin:
-        return os.path.join(binpath, '{}.exe'.format(execbin))
-    else:
-        return os.path.join(binpath, execbin)
+from .oscompat import (
+    get_binpath,
+    get_exec_path,
+    exec_bin,
+)
 
 def main():
     prefix = sys.prefix
@@ -40,7 +34,7 @@ def main():
     execbin = newargv[0]
 
     if os.sep not in execbin:
-        execbin = get_exec(binpath, execbin)
+        execbin = get_exec_path(binpath, execbin)
 
     if not os.path.exists(execbin):
         print('vrun requires that the target executable exists.', file=sys.stderr)
@@ -48,21 +42,7 @@ def main():
         sys.exit(-1)
 
     try:
-        # Windows has to be special...
-        if os.name == 'nt':
-            import subprocess
-            exe = subprocess.Popen(
-                [execbin] + newargv[1:],
-                env=newenv,
-                universal_newlines=True
-            )
-            exe.communicate()
-            sys.exit(exe.returncode)
-        else:
-            # Sane OS's let you just execute a new binary that takes the place
-            # of the old:
-            os.execve(execbin, newargv, newenv)
-
+        exec_bin(execbin, newargv, newenv)
     except Exception as e:
         print('vrun was unable to execute the target executable.', file=sys.stderr)
         print('Executable: {}'.format(execbin), file=sys.stderr)
---
+++ src/vrun/oscompat.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+WIN = sys.platform == 'win32' or os.name == 'nt'
+
+if not WIN:
+    def get_binpath(prefix):
+        return os.path.join(prefix, 'bin')
+
+    def get_exec_path(binpath, execbin):
+        return os.path.join(binpath, execbin)
+
+    def exec_bin(execbin, argv, env):
+        os.execve(execbin, argv, env)
+
+else: # pragma: nocover
+    import subprocess
+
+    def get_binpath(prefix):
+        return os.path.join(prefix, 'Scripts')
+
+    def get_exec_path(binpath, execbin):
+        if '.exe' not in execbin:
+            return os.path.join(binpath, '{}.exe'.format(execbin))
+        else:
+            return os.path.join(binpath, execbin)
+
+    def exec_bin(execbin, argv, env):
+        exe = subprocess.Popen(
+            [execbin] + argv[1:],
+            env=env,
+            universal_newlines=True
+        )
+        exe.communicate()
+        sys.exit(exe.returncode)
