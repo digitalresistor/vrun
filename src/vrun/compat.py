9696b687a31a249fc228e58773ff55eacf8beaaa
Bert JW Regeer <bertjw@regeer.org>
2017-09-16 23:32:49 -0600

    Add a read_dict function if it does not exist
    This is used for testing purposes, but when we drop Py2 support we can
    more easily remove it here than worrying about removing a monkeypatch in
    the testing code.

--- src/vrun/compat.py
+++ src/vrun/compat.py
@@ -8,3 +8,20 @@ try:
     from configparser import ConfigParser
 except ImportError:
     from ConfigParser import SafeConfigParser as ConfigParser
+
+if not hasattr(ConfigParser, 'read_dict'):
+    def read_dict(self, dictionary, source='<dict>'):
+        for (section, options) in dictionary.items():
+            if (
+                section
+                not in {
+                    self.default_section if hasattr(self, 'default_section') 
+                    else 'DEFAULT' 
+                }
+            ):
+                self.add_section(section)
+
+            for (option, value) in options.items():
+                self.set(section, option, value)
+
+    ConfigParser.read_dict = read_dict
