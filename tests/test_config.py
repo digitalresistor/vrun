42a92130fc9d6f3358bb03a7ab56cdc5f20eb4d1
Bert JW Regeer <bertjw@regeer.org>
2017-09-16 23:09:50 -0600

    Add tests for ancillary functions

---
+++ tests/test_config.py
@@ -0,0 +1,92 @@
+import os
+
+import pytest
+
+from vrun import config
+from vrun.compat import ConfigParser
+
+
+@pytest.mark.parametrize('parts, result', [
+    (
+        ['simple'],
+        ['simple']
+    ),
+    (
+        ['multiple', 'simple'],
+        ['multiple', 'simple']
+    ),
+    (
+        ['with', '"quotes"'],
+        ['with', '"quotes"']
+    ),
+    (
+        ['"testing', 'quote', 'support"'],
+        ['testing quote support']
+    ),
+    (
+        ["'testing", 'quote', "support'"],
+        ['testing quote support']
+    ),
+    (
+        ['"testing', '\'quote', 'support"'],
+        ['testing \'quote support']
+    ),
+    (
+        ['"testing', '\'quote\'', 'support"'],
+        ['testing \'quote\' support']
+    ),
+    (
+        ['"testing', '\'quote', '\'support"'],
+        ['testing \'quote \'support']
+    ),
+    (
+        ['""'],
+        ['""']
+    ),
+    (
+        ['" ', ' "'],
+        ['   ']
+    ),
+])
+def test_quoted_combine(parts, result):
+    assert list(config.quoted_combine(parts)) == result
+
+
+@pytest.mark.parametrize('parts', [
+    ['"testing', '\'quote', '"support"'],
+    ['" ', '""'],
+    ['"test', '"ing'],
+])
+def test_quoted_combine_invalid(parts):
+    with pytest.raises(ValueError):
+        assert list(config.quoted_combine(parts))
+
+
+@pytest.mark.parametrize('folder, result', [
+    ('configtest', 'vrun.cfg'),
+    ('configtest/vrun_ini', 'vrun.ini'),
+    ('configtest/setup_cfg', 'setup.cfg'),
+    ('configtest/setup_cfg_no_section', None),
+])
+def test_find_config(folder, result):
+    curpath = os.path.dirname(os.path.realpath(__file__))
+
+    cwd = os.path.join(curpath, folder)
+
+    if result:
+        assert config.find_config(cwd).endswith(result)
+    else:
+        assert config.find_config(cwd) == result
+
+
+@pytest.mark.parametrize('folder, result', [
+    ('configtest', 'vrun.cfg'),
+    ('configtest/vrun_ini', 'vrun.ini'),
+    ('configtest/setup_cfg', 'setup.cfg'),
+])
+def test_config_from_file(folder, result):
+    curpath = os.path.dirname(os.path.realpath(__file__))
+    cwd = os.path.join(curpath, folder)
+
+    config_file = config.find_config(cwd)
+    assert isinstance(config.config_from_file(config_file), ConfigParser)
